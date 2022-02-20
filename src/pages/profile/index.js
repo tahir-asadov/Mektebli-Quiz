8beb75c8556c8e88402401c3f1c557be0c16c1a8
Tahir Əsədov <tahir-asadov@outlook.com>
2022-02-20 16:08:10 +0400

    Add titles

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -14,13 +14,13 @@ export default function Profile( { user } ) {
 
       { user && <div>
 
-        <div><b>Имя: </b>&nbsp;{ user.firstName } </div>
+        <div title="Имя"><b>Имя: </b>&nbsp;{ user.firstName } </div>
 
-        <div><b>Фамилия: </b>&nbsp;{ user.lastName } </div>
+        <div title="Фамилия"><b>Фамилия: </b>&nbsp;{ user.lastName } </div>
 
-        <div><b>Публичное имя: </b>&nbsp;{ user.displayName } </div>
+        <div title="Публичное имя"><b>Публичное имя: </b>&nbsp;{ user.displayName } </div>
 
-        <div><b>Электронная почта: </b>&nbsp;{ user.email } </div>
+        <div title="Электронная почта"><b>Электронная почта: </b>&nbsp;{ user.email } </div>
 
       </div>
 
